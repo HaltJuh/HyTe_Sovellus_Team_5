937e0437aa7d1f14552ad001e1801c4c31f1ede7
Tino K <55874401+Mahtisaurus@users.noreply.github.com>
2020-05-02 14:08:09 +0300

    Muscle Building program with week list!
    Week list to create a plan!

--- Projekti/app/src/main/java/com/example/hyte_projekti/BuildMuscle.java
+++ Projekti/app/src/main/java/com/example/hyte_projekti/BuildMuscle.java
@@ -2,7 +2,9 @@ package com.example.hyte_projekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BuildMuscle extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class BuildMuscle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build_muscle);
+
+
+    }
+    public void goToMuscleDayList(View view) {
+        Intent intentMuscleDayList = new Intent(this, muscleDayList.class);
+        startActivity(intentMuscleDayList);
     }
 }
---
+++ Projekti/app/src/main/java/com/example/hyte_projekti/muscleDayList.java
@@ -0,0 +1,46 @@
+package com.example.hyte_projekti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class muscleDayList extends AppCompatActivity {
+
+    public static final String EXTRA = "MESSAGE";
+    private int latestActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muscle_day_list);
+        //Bundle b = getIntent().getExtras();
+        //int i = b.getInt(muscleDayList.EXTRA, 0);
+
+        SharedPreferences prefGet = getSharedPreferences(MainActivity.KEY, Activity.MODE_PRIVATE);
+        latestActivity = prefGet.getInt(MainActivity.LATESTACTIVITY, 0);
+        ListView lv = findViewById(R.id.muscleDayList);
+        lv.setAdapter(new ArrayAdapter<Days>(
+                this,
+                R.layout.days_item_layout,
+                DaysList.getInstance().getDays()
+        ));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent nextActivity = new Intent(muscleDayList.this, ExActivityThree.class);
+                    nextActivity.putExtra(EXTRA, i);
+                    startActivity(nextActivity);
+
+            }
+        });
+
+    }
+}
