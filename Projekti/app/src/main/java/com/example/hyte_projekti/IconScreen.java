ff28b05c928d93c69beedf8fcd5063a8979f6bd3
Tino K <55874401+Mahtisaurus@users.noreply.github.com>
2020-05-03 16:29:21 +0300

    Visual testing of UI design and Logo
    Logo!

---
+++ Projekti/app/src/main/java/com/example/hyte_projekti/IconScreen.java
@@ -0,0 +1,14 @@
+package com.example.hyte_projekti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IconScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icon_screen);
+    }
+}
